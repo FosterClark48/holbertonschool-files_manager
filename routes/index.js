26e78760f7dedce875c7a495568e09d791c3a5ce
NikkiMerena <alderman.nikki1022@gmail.com>
2023-11-19 16:36:47 -0600

    task3

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,9 @@
 // Central routing file, importing and consolidating routes from various controllers
+import express from 'express';
+import UsersController from '../controllers/UsersController';
+
+const router = express.Router();
+
+router.post('/users', UsersController.postNew);
+
+export default router;
