0d1de00320f9ec190e3a0aeff90fce66c9527595
FosterClark48 <fosterclark48@yahoo.com>
2023-11-20 16:51:32 -0600

    changed spacing from 4 to 2

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,29 +3,29 @@ import crypto from 'crypto';
 import dbClient from '../utils/db';
 
 class UsersController {
-    static async postNew(req, res) {
-        const { email, password } = req.body;
+  static async postNew(req, res) {
+    const { email, password } = req.body;
 
-        // Check for missing email or password
-        if (!email) return res.status(400).json({ error: 'Missing email' });
-        if (!password) return res.status(400).json({ error: 'Missing password'});
+    // Check for missing email or password
+    if (!email) return res.status(400).json({ error: 'Missing email' });
+    if (!password) return res.status(400).json({ error: 'Missing password' });
 
-        // check if email already exists
-        const existingUser = await dbClient.db.collection('users').findOne({ email });
-        if (existingUser) return res.status(400).json({ error: 'Already exist' });
+    // check if email already exists
+    const existingUser = await dbClient.db.collection('users').findOne({ email });
+    if (existingUser) return res.status(400).json({ error: 'Already exist' });
 
-        // Hash password with SHA1
-        const hash = crypto.createHash('sha1').update(password).digest('hex');
+    // Hash password with SHA1
+    const hash = crypto.createHash('sha1').update(password).digest('hex');
 
-        // Insert new user into database
-        const newUser = await dbClient.db.collection('users').insertOne({
-            email,
-            password: hash,
-        });
+    // Insert new user into database
+    const newUser = await dbClient.db.collection('users').insertOne({
+      email,
+      password: hash,
+    });
 
-        // Return new user info
-        return res.status(201).json({ id: newUser.insertedId, email });
-    }
+    // Return new user info
+    return res.status(201).json({ id: newUser.insertedId, email });
+  }
 }
 
 export default UsersController;
