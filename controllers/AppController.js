1a15c7c9eef3f7f69213c9df65c21ec5fc5a86fd
FosterClark48 <fosterclark48@yahoo.com>
2023-11-19 23:46:21 -0600

    Created AppController class. defined getStatus method to return status of Redis and database. defined getStats method to return the number of users and files in database

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,1 +1,19 @@
 // Controller for general application operations like checking service status
+const RedisClient = require('../utils/redis');
+const DBClient = require('../utils/db');
+
+class AppController {
+  static async getStatus(req, res) {
+    const redisAlive = RedisClient.isAlive();
+    const dbAlive = DBClient.isAlive();
+    res.status(200).send({ redis: redisAlive, db: dbAlive });
+  }
+
+  static async getStats(req, res) {
+    const usersCount = await DBClient.nbUsers();
+    const filesCount = await DBClient.nbFiles();
+    res.status(200).send({ users: usersCount, files: filesCount });
+  }
+}
+
+module.exports = AppController;
