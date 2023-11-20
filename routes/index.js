6c63bc4354c7fa3ebace26a65318ae44c0916168
FosterClark48 <fosterclark48@yahoo.com>
2023-11-19 23:45:26 -0600

    Defined API endpoints and linked routes to AppController methods getStatus and getStats

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,11 @@
 // Central routing file, importing and consolidating routes from various controllers
+const express = require('express');
+
+const AppController = require('../controllers/AppController');
+
+const router = express.Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+module.exports = router;
