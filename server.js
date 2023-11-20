f4e44fe742e0c5f5b4784b3885f026f70f412e50
FosterClark48 <fosterclark48@yahoo.com>
2023-11-19 23:44:26 -0600

    Initialized Express server. Set server to listen on env var PORT defaulted to 5000. Imported and used routes defined in routes/index.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 // Entry point of the application, setting up the server and middleware
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const routes = require('./routes/index');
+
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+module.exports = app;
