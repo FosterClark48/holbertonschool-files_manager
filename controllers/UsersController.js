fa4b20fa29ddcc3a8ec9b584f926768a88d97cec
FosterClark48 <fosterclark48@yahoo.com>
2023-11-21 01:35:36 -0600

    Added getMe method to retrieve the user base on the token used

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,8 @@
 // Controller for user-related operations (user creation and management)
 const crypto = require('crypto');
+const { ObjectID } = require('mongodb');
 const dbClient = require('../utils/db');
+const RedisClient = require('../utils/redis');
 
 class UsersController {
   static async postNew(req, res) {
@@ -25,6 +27,36 @@ class UsersController {
 
     // Return new user info
     return res.status(201).json({ id: newUser.insertedId, email });
+  }
+
+  // retrieve the user base on the token used
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    try {
+      // Get user ID string associated w/ token from Redis
+      const userIdString = await RedisClient.get(`auth_${token}`);
+      if (!userIdString) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      // Convert user ID string to MongoDB's ObjectID
+      const userId = new ObjectID(userIdString);
+      // Find user by ID from db
+      const user = await dbClient.users.findOne({ _id: userId });
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      // Return users ID and email, converting it from ObjectID to string for response
+      return res.status(200).json({ id: user._id.toString(), email: user.email });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 }
 
